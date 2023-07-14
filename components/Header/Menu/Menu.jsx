167de68f3f5e0ae8535c06274c219755ffa6c685
Ksel Ivan <Kselivan98@gmail.com>
2023-07-14 19:19:51 +0500

    base header complite

--- components/Header/Menu/Menu.jsx
+++ components/Header/Menu/Menu.jsx
@@ -1,27 +1,71 @@
+import { useState } from 'react';
 import style from './Menu.module.scss';
+
+const menuItems = ['Demos', 'Post', 'Features', 'Categories', 'Shop', 'Buy Now'];
+const subMenuItems = ['Post Header', 'Post Layout', 'Share Buttons', 'Gallery Post', 'Video Post'];
+
+const Option = (props) => {
+    const [show, setShow] = useState('');
+    const [border, setBorder] = useState('');
+
+    function handleChangeShow () {
+        show === '' ? setShow('--show') : setShow('');
+    }
+
+    return (
+        <>
+            <button 
+                className={style.menu__menuBtn}
+                onMouseEnter={handleChangeShow}
+                onMouseLeave={handleChangeShow}
+                // onClick={handleChangeShow}
+            >
+                <div className={style.menu__menuBtn__logo}>
+                    {props.item}
+                    <img src='/svg/bottomArrow.svg'/>
+                </div>
+                
+                <ul className={style[`menu__dropDawnMenu${show}`]}>
+                    {subMenuItems.map((item, index) => {
+                        if (index === subMenuItems.length - 1) {
+                            return (
+                                <li className={`${style.menu__list} ${style['menu__list--borderNon']}`}>
+                                    <p>
+                                        {item}
+                                        <img src='/svg/rightArrow.svg'/>
+                                    </p>
+                                </li>
+                            )
+                        } 
+
+                        return (
+                            <li className={style.menu__list}>
+                                <p>
+                                    {item}
+                                    <img src='/svg/rightArrow.svg'/>
+                                </p>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </button>
+        </>
+    )
+}
 
 const Menu = () => {
     return (
         <div className={style.menu}>
-            <ul>
-                <li>
-                    Demos                    
-                </li>
-                <li>
-                    Post
-                </li>
-                <li>
-                    Features
-                </li>
-                <li>
-                    Categories
-                </li>
-                <li>
-                    Shop
-                </li>
-                <li>
-                    Buy Now
-                </li>
+            <ul className={style.menu__nav}>
+                {menuItems.map((item) => {
+                    return (
+                        <il>
+                            <Option
+                                item={item}
+                            />
+                        </il>
+                    )
+                })}
             </ul>
         </div>
     )
